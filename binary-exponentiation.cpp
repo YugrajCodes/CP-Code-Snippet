a2002485775dce92810219196fa2244ef645fe1c
YugrajCodes <yugrajpersonal@gmail.com>
2025-04-27 05:05:33 +0530

    Update binary-exponentiation.cpp

--- binary-exponentiation.cpp
+++ binary-exponentiation.cpp
@@ -8,3 +8,15 @@ ll binExpo(ll a,ll b){
     }
     return res;
 }
+
+
+ll binExpoMod(ll a,ll b, ll m){
+    ll res = 1;
+    a = a % m;
+    while(b>0){
+        if(b&1) res = (res*a)%m;
+        a = (a*a)%m;
+        b>>=1;
+    }
+    return res;
+}
