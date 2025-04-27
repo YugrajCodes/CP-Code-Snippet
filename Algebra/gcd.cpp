86e76738f93f9dc9fe9b0d65860583548c665b52
YugrajCodes <yugrajpersonal@gmail.com>
2025-04-27 17:15:40 +0530

    added gcd

---
+++ Algebra/gcd.cpp
@@ -0,0 +1,22 @@
+
+ll gcd(ll a, ll b){
+    if(b==0) return a;
+    else return gcd(b,a%b);
+}
+
+
+//one line
+ll gcd(ll a, ll b){
+    return b ? gcd(b,a%b) : a ;
+}
+
+
+//loop
+
+ll gcd(ll a, ll b){
+    while(b){
+        a = a % b;
+        swap(a,b);
+    }
+    return a;
+}
